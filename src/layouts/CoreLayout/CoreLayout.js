424f0d10216eca021296e2c0485ec72b68ae8f43
Aditya Pratap Singh <adisinghrajput@gmail.com>
2017-02-20 22:18:16 +0100

    #29 Hide logout for now

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar'
-import FlatButton from 'material-ui/FlatButton'
-import firebase from 'firebase';
 import './CoreLayout.scss'
 import '../../styles/core.scss'
 
 export class CoreLayout extends Component {
-  logout () {
-		firebase.auth().signOut().then(function() {
-		// Sign-out successful.
-		}, function(error) {
-		// An error happened.
-		});
-	}
-
   render () {
     const { children } = this.props
     return (
       <div className='container text-center'>
-        <AppBar title='Build notifications' 
-          iconElementRight={<FlatButton label="logout" onClick={this.logout}/>}
-          />
+        <AppBar title='Build notifications'/>
         <div className='core-layout__viewport'>
           {children}
         </div>
